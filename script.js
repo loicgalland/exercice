448d19ca6a3dc8d2f0a4ea9360be26b118d3117a
loicgalland <loicgalland1@gmail.com>
2023-02-23 10:56:04 +0100

    Creation de fonctions pour créer des élements HTML

--- script.js
+++ script.js
@@ -1,50 +1,67 @@
 const response = await fetch("https://jsonplaceholder.typicode.com/albums");
-const etablissements = await response.json();
+const establishments = await response.json();
 
+for (let i = 0; i < establishments.length; i++) {
+    const hotel = establishments[i];
 
-
-
-
-      for (let i = 0; i < etablissements.length; i++) {
-    
-
-        const hotel = etablissements[i];
-
+    function createHotelImage(){
         const hotelImage = document.createElement('img');
         hotelImage.src = 'https://picsum.photos/100/100';
-    
+        return hotelImage;
+    }
+
+    function selectHotelCardRow(){
         const hotelCardRow = document.querySelector(".render-card");
+        return hotelCardRow;
+    }
+
+    
         const hotelContainer = document.createElement("div");
         hotelContainer.classList.add('col-3', 'mb-3');
+      
+
+    
         const hotelCard = document.createElement("div");   
         hotelCard.classList.add('card', 'h-100');
+
+ 
+
         const bodyHotelCard = document.createElement("div");
         bodyHotelCard.classList.add('card-body');
-    
-        const nomHotel = document.createElement('h1');
-        nomHotel.innerText = hotel.id;
-        nomHotel.classList.add('card-title');
-       
-        const desciptionHotel = document.createElement('p');
-        desciptionHotel.innerText = hotel.title;
-        desciptionHotel.classList.add('card-text');
-  
-    
-        const noteHotel = document.createElement('button');
-        noteHotel.innerText = hotel.userId;
-        noteHotel.classList.add('btn', 'btn-primary');
-    
-    
-        hotelCardRow.appendChild(hotelContainer);
-        hotelContainer.appendChild(hotelCard);
-        hotelCard.appendChild(hotelImage);
 
 
-        hotelCard.appendChild(bodyHotelCard);
-        bodyHotelCard.appendChild(nomHotel);
-        bodyHotelCard.appendChild(desciptionHotel);
-        bodyHotelCard.appendChild(noteHotel);
+
+    function createTitle(){
+        const hotelName = document.createElement('h1');
+        hotelName.innerText = hotel.id;
+        hotelName.classList.add('card-title');
+        return hotelName;
     }
+
+    function createDescritpion(){
+        const hotelDescription = document.createElement('p');
+        hotelDescription.innerText = hotel.title;
+        hotelDescription.classList.add('card-text');      
+        return hotelDescription;
+    }
+    
+    function createNote(){
+        const hotelButton = document.createElement('button');
+        hotelButton.innerText = hotel.userId;
+        hotelButton.classList.add('btn', 'btn-primary');
+        return hotelButton;
+    }
+
+
+
+    selectHotelCardRow().appendChild(hotelContainer);
+    hotelContainer.appendChild(hotelCard);
+    hotelCard.appendChild(createHotelImage());
+    hotelCard.appendChild(bodyHotelCard);
+    bodyHotelCard.appendChild(createTitle());
+    bodyHotelCard.appendChild(createDescritpion());
+    bodyHotelCard.appendChild(createNote());
+}
     
 
     
