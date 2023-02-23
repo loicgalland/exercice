1e166d9ff52ae5b96ed56c62e0b07ce41f73c563
loicgalland <loicgalland1@gmail.com>
2023-02-23 13:12:21 +0100

    Ajout des fonctions pour afficher les cards

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const response = await fetch("https://jsonplaceholder.typicode.com/albums");
 const establishments = await response.json();
 
-function createCardTitle(hotel){
+function createCardTitle(name){
     const hotelName = document.createElement('h1');
-    hotelName.innerText = hotel.id;
+    hotelName.innerText = name;
     hotelName.classList.add('card-title');
     return hotelName;
 }
 
-function createCardDescritpion(hotel){
+function createCardDescription(description){
     const hotelDescription = document.createElement('p');
-    hotelDescription.innerText = hotel.title;
+    hotelDescription.innerText = description;
     hotelDescription.classList.add('card-text');      
     return hotelDescription;
 }
 
-function createCardNote(hotel){
+function createCardNote(note){
     const hotelButton = document.createElement('button');
-    hotelButton.innerText = hotel.userId;
+    hotelButton.innerText = note;
     hotelButton.classList.add('btn', 'btn-primary');
     return hotelButton;
 }
@@ -29,29 +29,40 @@ function createCardImage(){
 }
 
 function selectHotelCardRow(){
-    const hotelCardRow = document.querySelector(".render-card");
-    return hotelCardRow;
+    return document.querySelector(".render-card");
+}
+
+function createCardContainer(){
+    const hotelContainer = document.createElement("div");
+    hotelContainer.classList.add('col-3', 'mb-3');
+    return hotelContainer;
+}
+
+function createCard(){
+    const hotelCard = document.createElement("div");   
+    hotelCard.classList.add('card', 'h-100');
+    return hotelCard;
+}
+
+function createCardBody(){
+    const bodyHotelCard = document.createElement("div");
+    bodyHotelCard.classList.add('card-body');
+    return bodyHotelCard;
 }
 
 for (let i = 0; i < establishments.length; i++) {
     const hotel = establishments[i];
-
-    const hotelContainer = document.createElement("div");
-    hotelContainer.classList.add('col-3', 'mb-3');
-
-    const hotelCard = document.createElement("div");   
-    hotelCard.classList.add('card', 'h-100');
-
-    const bodyHotelCard = document.createElement("div");
-    bodyHotelCard.classList.add('card-body');
+    const hotelContainer = createCardContainer(); 
+    const hotelCard = createCard();
+    const bodyHotelCard = createCardBody();
 
     selectHotelCardRow().appendChild(hotelContainer);
     hotelContainer.appendChild(hotelCard);
     hotelCard.appendChild(createCardImage());
     hotelCard.appendChild(bodyHotelCard);
-    bodyHotelCard.appendChild(createCardTitle(hotel));
-    bodyHotelCard.appendChild(createCardDescritpion(hotel));
-    bodyHotelCard.appendChild(createCardNote(hotel));
+    bodyHotelCard.appendChild(createCardTitle(hotel.id));
+    bodyHotelCard.appendChild(createCardDescription(hotel.title));
+    bodyHotelCard.appendChild(createCardNote(hotel.userId));
 }
     
 
